f8981f02a08fe39a4dfb921246fd74f1baf55009
Littleeich <Littleeich@gmail.com>
2021-05-13 16:23:04 +0300

    Simple number change

--- HT6/simpleNumbers.js
+++ HT6/simpleNumbers.js
@@ -2,6 +2,9 @@ maxNumber = 10
 sumOfSimpleNumbers = 0
 for (let i =2; i < maxNumber; i++) {
 	for (let j = 2; j < i; j++) {
+		if ( i == 2) {
+			sumOfSimpleNumbers += i
+		}
 		if (i % j == 0) {
 			break
 		}
